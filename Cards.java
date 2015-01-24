fe16303c6d23ba8be20cc11957b74561367c0031
Niloufar1171 <Niloufar.behrouzi@yahoo.com>
2015-01-24 11:12:24 -0800

    Create Cards.java

---
+++ Cards.java
@@ -0,0 +1,10 @@
+
+public class Cards {
+	public static final int Personality=1;
+	public static final int Event=2;
+	public static final int CityArea=3;
+	public static final int BoardPlayer=4;
+	
+	public string FaceValue ;
+
+}
